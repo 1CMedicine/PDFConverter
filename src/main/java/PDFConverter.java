626199560626b588739e13f995e3258febdc949a
Alexey Gaidukov <alexey.gaidukov@hotmail.com>
2023-07-15 11:50:03 +0300

    добалено получение логов

--- src/main/java/PDFConverter.java
+++ src/main/java/PDFConverter.java
@@ -12,12 +12,16 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.stream.Collectors;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Random;
 
 import java.io.OutputStream;
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import com.openhtmltopdf.extend.FSCacheEx;
 import com.openhtmltopdf.extend.FSCacheValue;
 import com.openhtmltopdf.extend.impl.FSDefaultCacheStore;
+import com.openhtmltopdf.util.*;
 
 import org.jsoup.Jsoup;
 import org.jsoup.helper.W3CDom;
@@ -25,11 +29,13 @@ import org.jsoup.helper.W3CDom;
 
 public class PDFConverter extends HttpServlet {
 
+    final int logKeyLength = 5;
     final static Logger log = LogManager.getLogger(PDFConverter.class.getName());
     private static final MultipartConfigElement MULTI_PART_CONFIG = new MultipartConfigElement(System.getProperty("java.io.tmpdir"));
     protected String DATA_PATH;
     FSCacheEx<String, FSCacheValue> fsCacheEx = new FSDefaultCacheStore();
     List<AutoFont.CSSFont> fonts;
+    ConcurrentHashMap<String, List<Diagnostic> > lastLog = new ConcurrentHashMap<>();
 
     public void init() throws ServletException {
         super.init();
@@ -61,6 +67,9 @@ public class PDFConverter extends HttpServlet {
         if (req.getServletPath().startsWith("/send_file.html")) { 
             log.info(req.getServletPath());
             send_file(req.getContextPath(), resp);
+        } else if (req.getServletPath().startsWith("/log_") && req.getServletPath().length() == logKeyLength+5) { 
+            log.info(req.getServletPath());
+            getLog(req, resp);
         } else {
             log.warn("PAGE NOT FOUND: "+req.getServletPath());
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
@@ -82,6 +91,22 @@ public class PDFConverter extends HttpServlet {
         }
     }
 
+    private void initLogging(HttpServletRequest req, PdfRendererBuilder builder, String logging) {
+        if (logging.length() == logKeyLength) {
+            XRLog.setLoggingEnabled(true);
+            List<Diagnostic> l = lastLog.get(logging);
+            if (l == null) {
+                l = new ArrayList<>();
+                lastLog.put(logging, l);
+            } else {
+                l.clear();
+            }
+            builder.withDiagnosticConsumer(l::add);
+        } else {
+            XRLog.setLoggingEnabled(false);
+        }
+    }
+
     private void convertStrictHtml(HttpServletRequest req, HttpServletResponse resp) 
             throws IOException, ServletException {
 
@@ -89,6 +114,7 @@ public class PDFConverter extends HttpServlet {
             req.setAttribute(Request.MULTIPART_CONFIG_ELEMENT, MULTI_PART_CONFIG);
         }
         Part filePart = req.getPart("file");
+        final String logging = req.getParameter("logging"); 
         InputStream fileContent = filePart.getInputStream();
 
         resp.setContentType("application/pdf");
@@ -98,6 +124,7 @@ public class PDFConverter extends HttpServlet {
         String html = new BufferedReader(new InputStreamReader(fileContent, "UTF-8")).lines().collect(Collectors.joining("\n"));
 
         PdfRendererBuilder builder = new PdfRendererBuilder();
+        initLogging(req, builder, logging);
         builder.useCacheStore(PdfRendererBuilder.CacheStore.PDF_FONT_METRICS, fsCacheEx);
         AutoFont.toBuilder(builder, fonts);
         builder.useFastMode();
@@ -119,6 +146,7 @@ public class PDFConverter extends HttpServlet {
             req.setAttribute(Request.MULTIPART_CONFIG_ELEMENT, MULTI_PART_CONFIG);
         }
         Part filePart = req.getPart("file");
+        final String logging = req.getParameter("logging"); 
         InputStream fileContent = filePart.getInputStream();
 
         resp.setContentType("application/pdf");
@@ -130,6 +158,7 @@ public class PDFConverter extends HttpServlet {
 		org.w3c.dom.Document dom = new W3CDom().fromJsoup(doc);
 
         PdfRendererBuilder builder = new PdfRendererBuilder();
+        initLogging(req, builder, logging);
         builder.useCacheStore(PdfRendererBuilder.CacheStore.PDF_FONT_METRICS, fsCacheEx);
         AutoFont.toBuilder(builder, fonts);
         builder.useFastMode();
@@ -144,18 +173,36 @@ public class PDFConverter extends HttpServlet {
         output.close();
     }
 
+    private String generateRandomString() {
+        final int leftLimit = 48; // numeral '0'
+        final int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+    
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+          .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+          .limit(logKeyLength)
+          .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+          .toString();
+    
+        return generatedString;
+    }
+
     private void send_file(final String contextPath, HttpServletResponse resp) 
             throws IOException {
 
         resp.setHeader("Content-Type", "text/html; charset=UTF-8");
         PrintWriter out = resp.getWriter();
+        final String logKey = generateRandomString();
         out.print(String.join("\n"
         , "<head>"
-        , "<title>Конвертация одного html или пакета html файлов в pdf</title>"
+        , "<title>Конвертация html в pdf</H2></title>"
         , "</head>"
         , "<body>"
-        , "<H2>Конвертация одного xhtml (в utf8) в pdf</H2>"
+        , "<H2>Конвертация одного xhtml (в utf8) в pdf</H2></H2>"
         , "<form enctype = 'multipart/form-data' method='post' action='", contextPath, "/convertStrictHtml'>"
+        , "  <fieldset>"
+        , "   <p><label for='logging'>Ключ лога. Пусто - нет логирования:</label><input type='text' name='logging' value='"+logKey+"'></p>"
+        , "  </fieldset>"
         , "  <p>"
         , "   <input type='file' name='file'>"
         , "   <input type='submit' value='Отправить xhtml'>"
@@ -164,11 +211,49 @@ public class PDFConverter extends HttpServlet {
         , "<hr>"
         , "<H2>Конвертация одного html5 (в utf8) в pdf</H2>"
         , "<form enctype = 'multipart/form-data' method='post' action='", contextPath, "/convertHtml5'>"
+        , "  <fieldset>"
+        , "   <p><label for='logging'>Ключ лога. Пусто - нет логирования:</label><input type='text' name='logging' value='"+logKey+"'></p>"
+        , "  </fieldset>"
         , "  <p>"
         , "   <input type='file' name='file'>"
         , "   <input type='submit' value='Отправить html5'>"
         , "  </p>"
         , "</form>"
+        , "<br>"
+        , "<a href='", contextPath, "/log_"+logKey+"'>Диагностика последнего вызова openhtmltopdf</a>"
+        , "</body>"
+        , "</html>"));
+    }
+
+    private void getLog(HttpServletRequest req, HttpServletResponse resp) 
+            throws IOException {
+
+        resp.setHeader("Content-Type", "text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.print(String.join("\n"
+        , "<head>"
+        , "<title>Диагностика openhtmltopdf</title>"
+        , "</head>"
+        , "<body>"
+        , "<H2>Диагностика openhtmltopdf</H2>"));
+
+        String key = req.getServletPath().substring(5);
+        List<Diagnostic> l = lastLog.get(key);
+        lastLog.remove(key);
+        out.print("<p>Ключ лога - "+key+"</p>");
+        if (l == null) {
+            out.print("<p>Логирование выключено</p>");
+        } else {
+            for (Diagnostic diag : l) {
+                if (diag.getLevel() != java.util.logging.Level.FINEST && diag.getLevel() != java.util.logging.Level.FINER) {
+                    out.print(diag.getLevel());
+                    out.print(" ");
+                    out.print(diag.getFormattedMessage());
+                    out.print("<br>");
+                }
+            }
+        }
+        out.print(String.join("\n"
         , "</body>"
         , "</html>"));
     }
